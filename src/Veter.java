3fb7dc74a6da506c0bda69a6bcb785eb1be217e8
VeterAlex <twinzata@gmail.com>
2015-09-20 10:10:07 +0300

    new file

---
+++ src/Veter.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Ukrainian IT_School on 20.09.2015.
+ */
+public class Veter {
+    public static void main(String args[]) {
+        System.out.println("Привет народ!");
+
+    }
+}
